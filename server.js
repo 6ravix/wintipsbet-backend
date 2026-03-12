247765c379204a6a0a86f8954caac2affbab5808
kelvin njogu <kelvinnjogu19153@gmail.com>
2026-03-13 00:16:07 +0300

    Initial WinTipsBet backend

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_URI)
+
+app.get("/", (req, res) => {
+  res.send("WinTipsBet API running")
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT)
+})
+CTRL + X
+Y
+ENTER
+
